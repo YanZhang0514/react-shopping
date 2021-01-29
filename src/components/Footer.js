7bbc7f938b80315ef54cf07f73ac9bb400478e91
YanZhang0514 <yanzhang05@outlook.com>
2021-01-29 07:48:46 -0500

    Footer Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,12 @@ const Footer = (props) => {
   return (
     <footer>
       <p className="footer-links">
-        <a
-          href="https://github.com/yanzhang05/react-shopping-cart"
-          target="_blank"
-        >
+        <a href="https://github.com/yanzhang05/react-shopping" target="_blank">
           View Source on Github
         </a>
         <span> / </span>
-        <a href="mailto:contact@yanzhang05.in" target="_blank">
+        <a href="mailto:contact@yanzhang05@outlook.com" target="_blank">
           Need any help?
-        </a>
-        <span> / </span>
-        <a href="https://twitter.com/Nyanzhang05" target="_blank">
-          Say Hi on Twitter
-        </a>
-        <span> / </span>
-        <a href="https://yanzhang05.in" target="_blank">
-          Read My Blog
         </a>
       </p>
       <p>
